dfd271178f1b68b016d06116daf761ff0f36f067
rim <73057243+RimELMGHARI@users.noreply.github.com>
2021-04-02 22:31:03 +0100

    Update kata01.py

--- module00/ex05/kata01.py
+++ module00/ex05/kata01.py
@@ -1,1 +1,10 @@
 
+languages = {
+    'Python': 'Guido van Rossum',
+    'Ruby': 'Yukihiro Matsumoto',
+    'PHP': 'Rasmus Lerdorf',
+    }
+
+print ("Python was created by ", languages['Python'])
+print ("Ruby was created by ", languages['Ruby'])
+print ("PHP was created by ", languages['PHP'])
