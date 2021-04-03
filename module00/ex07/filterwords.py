0ac06b880bb3f16b5876d0ef0048b649c1141143
rim <73057243+RimELMGHARI@users.noreply.github.com>
2021-04-03 18:55:02 +0100

    Update filterwords.py

--- module00/ex07/filterwords.py
+++ module00/ex07/filterwords.py
@@ -1,1 +1,9 @@
-
+def filterWords(text, n):
+    L=[]
+    text=text.split()
+    for i in range(len(text)):
+        if len(text[i])>=n:
+            L.append(text[i])
+    
+    return L
+            
